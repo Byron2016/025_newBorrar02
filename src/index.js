6363b9e327faf9fe95ff8aed9ba4a38732eea4ce
Byron <bgva2005@yahoo.com>
2023-04-18 16:45:31 -0500

    rutas agregadas

--- src/index.js
+++ src/index.js
@@ -1,20 +1,29 @@
 // index.js
-const express = require('express')
+const express = require("express");
 
-const app = express()
-const PORT = 4000
+const app = express();
+const PORT = 4000;
 
 app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `)
-})
+  console.log(`API listening on PORT ${PORT} `);
+});
 
-app.get('/', (req, res) => {
-  res.send('Hey this is my API running 🥳')
-})
+// Routes
+app.get("/", (req, res) => {
+  res.send("Hey this is my API running 🥳");
+});
 
-app.get('/about', (req, res) => {
-  res.send('This is my about route..... ')
-})
+app.get("/about", (req, res) => {
+  res.send("This is my about route..... ");
+});
+
+app.get("/test", (req, res) => {
+  return res.send("NewBorrar_01 Express Typescript on Vercel");
+});
+
+app.get("/ping", (req, res) => {
+  return res.send("MNewBorrar_01 pong.. --> 🏓");
+});
 
 // Export the Express API
-module.exports = app
+module.exports = app;
